878f77c4521fb1201e2c10277c7dac3b962e9980
omar <omarbek1122@gmail.com>
2025-08-19 22:45:24 +0300

    Fix white screen

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -30,7 +30,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "916c49d1bcbaae68e91bff6195a4bf8e
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "a8b7d4747f6033735a427c23b65633e3",
+"flutter_bootstrap.js": "958c76bfb6a06c83ac051db43856e138",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
