c90a7cb0656250a4156ddc98bd5b14dbf0a5541b
omar <omarbek1122@gmail.com>
2025-08-20 02:05:51 +0300

    Fix white screen

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"39d6d6e699e51b2874210e14cddf1a22fb9524
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "691928540"
+    serviceWorkerVersion: "1135902246"
   }
 });
