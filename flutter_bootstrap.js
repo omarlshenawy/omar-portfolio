187b3a7e577fb25fa03e67bcd0e93e451fa07dfe
omarlshenawy <omar.bek1122@gmail.com>
2026-05-06 04:42:42 +0300

    Deploy Flutter web to GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1317471646" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4115569267" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
